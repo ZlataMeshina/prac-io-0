2fef5eeb9d52486eb6db9a66eaa1c155522a0a36
ZlataMeshina <31792375+ZlataMeshina@users.noreply.github.com>
2017-09-24 00:54:11 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Злата on 21.09.17.
+//  Copyright © 2017 Злата. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
